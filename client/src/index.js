dd44c1f6e5f2f7bbbb6fe1ac9cd289075aec4ff7
Qabas <qabasalani1@gmail.com>
2026-02-05 22:36:31 +0700

    Migrate from Heroku to Render + Netlify deployment
    - Replace Heroku URLs with environment variables in client/src/index.js
    - Add .env.production for Netlify deployment with Render backend URLs
    - Update .gitignore with React build folders and local environment files
    - Backend deployed to Render: https://comic-space-backend.onrender.com
    - Frontend deployed to Netlify: https://comic-space-client.netlify.app
    
    This migration demonstrates:
    - Cloud infrastructure adaptability
    - Environment variable management
    - Modern deployment pipeline (Netlify + Render)
    - Professional portfolio presentation

--- client/src/index.js
+++ client/src/index.js
@@ -1,33 +1,102 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-} from "@apollo/client";
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   ApolloProvider,
+// } from "@apollo/client";
+// import { setContext } from "@apollo/client/link/context";
+// import { split, HttpLink } from '@apollo/client';
+// import { getMainDefinition } from '@apollo/client/utilities';
+// import { WebSocketLink } from '@apollo/client/link/ws';
+// import { Provider } from 'react-redux';
+// import store from '../src/utils/store';
+
+// const wsLink = new WebSocketLink({
+//   uri: process.env.NODE_ENV === 'production'
+//     ? 'wss://young-hollows-20691.herokuapp.com/graphql'
+//     : 'ws://localhost:3001/graphql',
+//   options: {
+//     reconnect: true
+//   }
+// });
+
+// const httpLink = new HttpLink({
+//   uri: process.env.NODE_ENV === 'production'
+//     ? 'https://young-hollows-20691.herokuapp.com/graphql'
+//     : 'http://localhost:3001/graphql',
+// });
+
+// const authLink = setContext((_, { headers }) => {
+//   const token = localStorage.getItem("id_token");
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : "",
+//     },
+//   };
+// });
+
+// const splitLink = split(
+//   ({ query }) => {
+//     const definition = getMainDefinition(query);
+//     return (
+//       definition.kind === 'OperationDefinition' &&
+//       definition.operation === 'subscription'
+//     );
+//   },
+//   wsLink,
+//   authLink.concat(httpLink)
+// );
+
+// const client = new ApolloClient({
+//   link: splitLink,
+//   cache: new InMemoryCache(),
+// });
+
+// ReactDOM.render(
+//   <>
+//     <ApolloProvider client={client}>
+//       <Provider store={store}>
+//         <App />
+//       </Provider>
+//     </ApolloProvider>
+//   </>,
+//   document.getElementById('root')
+// );
+
+// reportWebVitals();
+
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
-import { split, HttpLink } from '@apollo/client';
-import { getMainDefinition } from '@apollo/client/utilities';
-import { WebSocketLink } from '@apollo/client/link/ws';
-import { Provider } from 'react-redux';
-import store from '../src/utils/store';
+import { split, HttpLink } from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { Provider } from "react-redux";
+import store from "../src/utils/store";
+
+// Get API URLs from environment variables
+const WS_URL = process.env.REACT_APP_WS_URL || "ws://localhost:3001/graphql";
+const HTTP_URL =
+  process.env.REACT_APP_API_URL || "http://localhost:3001/graphql";
 
 const wsLink = new WebSocketLink({
-  uri: process.env.NODE_ENV === 'production'
-    ? 'wss://young-hollows-20691.herokuapp.com/graphql'
-    : 'ws://localhost:3001/graphql',
+  uri: WS_URL,
   options: {
-    reconnect: true
-  }
+    reconnect: true,
+  },
 });
 
 const httpLink = new HttpLink({
-  uri: process.env.NODE_ENV === 'production'
-    ? 'https://young-hollows-20691.herokuapp.com/graphql'
-    : 'http://localhost:3001/graphql',
+  uri: HTTP_URL,
 });
 
 const authLink = setContext((_, { headers }) => {
@@ -40,17 +109,16 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
-
 const splitLink = split(
   ({ query }) => {
     const definition = getMainDefinition(query);
     return (
-      definition.kind === 'OperationDefinition' &&
-      definition.operation === 'subscription'
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
     );
   },
   wsLink,
-  authLink.concat(httpLink)
+  authLink.concat(httpLink),
 );
 
 const client = new ApolloClient({
@@ -66,7 +134,7 @@ ReactDOM.render(
       </Provider>
     </ApolloProvider>
   </>,
-  document.getElementById('root')
+  document.getElementById("root"),
 );
 
 reportWebVitals();
